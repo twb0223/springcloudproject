e71b55bd9e23927b0f2edc070a6d324eaa4fa263
twb0223 <twb0223@gmail.com>
2017-12-06 16:08:01 +0800

    add configserver

--- SpringCloudProject/service-ribbon/src/main/java/com/twb/serviceribbon/controller/HelloController.java
+++ SpringCloudProject/service-ribbon/src/main/java/com/twb/serviceribbon/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloControler {
+public class HelloController {
     @Autowired
     HelloService helloService;
 
--- SpringCloudProject/service-ribbon/src/main/java/com/twb/serviceribbon/service/HelloService.java
+++ SpringCloudProject/service-ribbon/src/main/java/com/twb/serviceribbon/service/HelloService.java
@@ -18,4 +18,3 @@ public class HelloService {
         return "hi,"+name+",sorry,error!";
     }
 }
-
